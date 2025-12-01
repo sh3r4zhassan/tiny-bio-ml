5e8c9e0ed8c85dcd4263fcccf291537d781f5613
Sheraz Hassan <sherazhassan358@gmail.com>
2025-12-01 01:10:16 -0500

    Fix white page path issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tiny-bio-ml/", // <--- This assumes your repo name is 'tiny-bio-ml'
+  base: "./", // <--- This assumes your repo name is 'tiny-bio-ml'
 })
